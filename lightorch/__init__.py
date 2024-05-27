6648573d290db0febd952843237024c45b133791
Jorgedavyd <jorged.encyso@gmail.com>
2024-05-27 12:22:23 -0400

    lightorch 0.0.1

--- lightorch/__init__.py
+++ lightorch/__init__.py
@@ -2,7 +2,7 @@ from .htuning import *
 from .nn import *
 from .training import *
 from .deprecated import *
-from _version import __version__
+from ._version import __version__
 
 __author__ = "Jorge David Enciso Martínez"
 __email__ = "jorged.encyso@gmail.com"
