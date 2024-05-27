208e8d0f628ebfee3ad7913c7e383404af0c22a8
Jorgedavyd <jorged.encyso@gmail.com>
2024-05-27 12:16:07 -0400

    beta solving version

--- lightorch/nn/transformer/transformer.py
+++ lightorch/nn/transformer/transformer.py
@@ -1,41 +1,39 @@
-"""INCLUDE ARCH FOR GENERAL"""
-
 from torch import nn, Tensor
 from typing import Optional, List
 
-
 class GeneralTransformer(nn.Module):
-    def __init__(
-        self,
-        *,
-        encoder: Optional[nn.Module],
-        decoder: Optional[nn.Module],
-        n_layers: int
-    ) -> None:
+    def __init__(self, *, encoder: Optional[nn.Module], decoder: Optional[nn.Module], n_layers: int) -> None:
         super().__init__()
         if encoder is not None:
 
-            self.encoders = nn.Sequential(*[encoder for _ in range(n_layers)])
+            self.encoders = nn.Sequential(
+                *[
+                    encoder for _ in range(n_layers)
+                ]
+            )
         else:
             self.encoders = False
 
         if decoder is not None:
-            self.decoders = nn.Sequential(*[decoder for _ in range(n_layers)])
+            self.decoders = nn.Sequential(
+                *[
+                    decoder for _ in range(n_layers)
+                ]
+            )
         else:
             self.decoders = False
-
     def encoder_forward(self, x: Tensor) -> Tensor:
         hist: List[nn.Module] = []
         for module in self.encoders:
             x = module(x)
             hist.append(x)
         return hist
-
     def forward(self, x: Tensor) -> Tensor:
         if self.encoders:
             hist = self.encoder_forward(x)
         # Continue
-
+            
+        
 
 class CrossTransformer(nn.Module):
     def __init__(self, encoder: nn.Module, decoder: nn.Module, n_layers: int) -> None:
