33d6082961f1e128b16eeb702949ae0c57baf0a8
Jorgedavyd <jorged.encyso@gmail.com>
2024-05-28 19:41:29 -0400

    ci-cd debug

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,13 @@
-rm requirements.txt
+#!/bin/bash
+
+# Remove existing requirements.txt if it exists
+rm -f requirements.txt
+
+# Generate requirements.in from the lightorch directory
 pipreqs lightorch/ --savepath=requirements.in
-pip-compile
-rm requirements.in
+
+# Compile requirements.in to requirements.txt
+pip-compile requirements.in
+
+# Clean up
+rm -f requirements.in
