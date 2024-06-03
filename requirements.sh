4c0da4ba6b0f1ff1e847d3c4958f40924103f34e
Jorgedavyd <jorged.encyso@gmail.com>
2024-06-02 20:02:12 -0400

    ci-cd

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-pipreqs lightorch --savepath=requirements.in
+PROJECT_DIR="lightorch"
+REQUIREMENTS_IN="requirements.in"
+REQUIREMENTS_TXT="requirements.txt"
 
-pip-compile requirements.in
+# Generate requirements.in using pipreqs
+echo "Generating ${REQUIREMENTS_IN} with pipreqs..."
+pipreqs $PROJECT_DIR --savepath=$REQUIREMENTS_IN
 
-rm -f requirements.in
+# Check if pipreqs succeeded
+if [[ $? -ne 0 ]]; then
+    echo "pipreqs failed to generate ${REQUIREMENTS_IN}. Exiting."
+    exit 1
+fi
+
+# Compile requirements.in to requirements.txt using pip-compile
+echo "Compiling ${REQUIREMENTS_IN} to ${REQUIREMENTS_TXT} with pip-compile..."
+pip-compile $REQUIREMENTS_IN
+
+# Check if pip-compile succeeded
+if [[ $? -ne 0 ]]; then
+    echo "pip-compile failed to generate ${REQUIREMENTS_TXT}. Exiting."
+    exit 1
+fi
+
+# Remove the intermediate requirements.in file
+echo "Removing ${REQUIREMENTS_IN}..."
+rm -f $REQUIREMENTS_IN
+
+echo "Requirements successfully generated in ${REQUIREMENTS_TXT}."
