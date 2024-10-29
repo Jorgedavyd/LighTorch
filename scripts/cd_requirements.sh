b6e84fa6047135eda588ce54c2481bb062de4410
Jorgedavyd <jorged.encyso@gmail.com>
2024-10-29 17:36:27 -0300

    cd solved 5

--- scripts/cd_requirements.sh
+++ scripts/cd_requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 -m pip install --upgrade pip
 pip3 install pytest pipreqs pip-tools setuptools wheel
-pipreqs starstream --savepath=./requirements.in
+pipreqs lightorch --savepath=./requirements.in
 sed -i 's/==.*$//' requirements.in
 sort requirements.in | uniq > requirements_unique.in
 pip-compile ./requirements_unique.in
